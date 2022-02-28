fae0cbf639f658e4ae265b236f01f7f33661e83a
yt-li <yitong.li15@imperial.ac.uk>
2022-02-28 22:42:18 +0000

    Add simulation results of SG-load

--- TestData_K_Simulation/ScopePlot.m
+++ TestData_K_Simulation/ScopePlot.m
@@ -1,17 +1,20 @@
 % Scope plot
+close all
 
 clear('Time');
 clear('theta','theta_','theta__');
 
 Time = out.Data_App1{6}.Values.Time;
+TimeShift = 2;
+Time = Time - TimeShift;
 
 Fbus = [19,22,30,31,32,34,35,37,38,43,54,57,58,62,63,65,66];
 
-NumRef = 17;
-NumBus = 68;
+% NumRef = 17;
+% NumBus = 68;
 
-% NumRef = 1;
-% NumBus = 16;
+NumRef = 1;
+NumBus = 16;
 
 Enable_17InfBus = 0;
 
@@ -56,18 +59,25 @@ for i = 1:NumBus
 end
 
 figure(999)
+FigSize = [0.1 0.1 0.35 0.5];
+set(gcf,'units','normalized','outerposition',FigSize);
 for i = 1:NumBus
     if isempty(find(Fbus == i,1))
         subplot(2,1,1)
         plot(Time,dtheta{i}); hold on; grid on;
-        xlim([0.2,4.5]);
+        xlim([0,1.5]);
+        ylim([-0.5,0.3]);
         ylabel('Angle (rad)');
-        xlabel('Time (s)');
+        % xlabel('Time (s)');
         subplot(2,1,2)
         plot(Time,vm{i}); hold on; grid on;
-        %ylim([0,1.5]);
-        xlim([0.2,4.5]);
+        ylim([0,1.5]);
+        xlim([0,1.5]);
         ylabel('Voltage (pu)')
         xlabel('Time (s)')
     end
+end
+
+if 0
+    print(gcf,['K_Sim_SG_Load_1d5s.png'],'-dpng','-r600');
 end
